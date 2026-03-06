1edb478a03be09b94e2121c31e772f06687054b4
Иван Шихалев <shikhalev@gmail.com>
2026-03-06 13:39:44 +0500

    Пт 06 мар 2026 13:39:44 +05

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Скрипты статистики ([inat-get](https://github.com/inat-get/inat-get)) 
 для [районов Свердловской области](https://www.inaturalist.org/projects/bioraznoobrazie-rayonov-sverdlovskoy-oblasti).
+
++ Скрипты могут и будут меняться по мере развития как проекта **[🌿 iNatGet](https://github.com/inat-get/inat-get)**, так
+  указанных проектов на iNaturalist.
--- generate.rb
+++ generate.rb
@@ -1,2 +1,44 @@
 # frozen_string_literal: true
 
+require 'fileutils'
+
+require_relative 'lib/districts'
+
+FileUtils.mkdir_p 'generated'
+
+list = File.open 'generated.lst', 'w'
+
+DISTRICTS.each do |key, _|
+  File.open "generated/#{ key }.rb", 'w' do |file|
+    file.puts '# frozen_string_literal: true'
+    file.puts ''
+    file.puts "require_relative '../lib/report'"
+    file.puts ''
+    file.puts "make_district_reports '#{ key }'"
+  end
+  list.puts "generated/#{ key }.rb"
+end
+
+ZONES.each do |key, _|
+  File.open "generated/#{key}.rb", "w" do |file|
+    file.puts "# frozen_string_literal: true"
+    file.puts ''
+    file.puts "require_relative '../lib/report'"
+    file.puts ''
+    file.puts "make_aria_reports '#{ key }'"
+  end
+  list.puts "generated/#{key}.rb"
+end
+
+SPECIALS.each do |key, _|
+  File.open "generated/#{key}.rb", "w" do |file|
+    file.puts "# frozen_string_literal: true"
+    file.puts ''
+    file.puts "require_relative '../lib/report'"
+    file.puts ''
+    file.puts "make_special_reports '#{ key }'"
+  end
+  list.puts "generated/#{key}.rb"
+end
+
+list.close
